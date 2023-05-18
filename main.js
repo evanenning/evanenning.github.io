a8c54b95586902dcb4e58fcb66f1c058be78d193
Eva Nenning <118194409+evanenning@users.noreply.github.com>
2023-05-18 18:14:19 -0300

    prueba form validacion

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//footer//
+
 var footer = document.querySelector('footer');
 var lastScrollTop = 0;
 
@@ -9,4 +11,26 @@ window.addEventListener('scroll', function() {
     footer.classList.add('hidden');
   }
   lastScrollTop = scrollTop;
+});
+
+//formulario//
+
+document.getElementById("botonEnviar").addEventListener("click", function() {
+  
+  var nombreInput = document.querySelector("#formulario input[name='nombre']");
+  var emailInput = document.querySelector("#formulario input[name='email']");
+  var mensajeInput = document.querySelector("#formulario input[name='mensaje']");
+  
+  
+  if (nombreInput.value === "" || emailInput.value === "" || mensajeInput.value === "") {
+    alert("Por favor, complete todos los campos.");
+    return; 
+  }
+  
+ 
+  
+  
+  nombreInput.value = "";
+  emailInput.value = "";
+  mensajeInput.value = "";
 });

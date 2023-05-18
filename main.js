6403e3b7d80c9bd3542a2fefdf37520962612fab
Eva Nenning <118194409+evanenning@users.noreply.github.com>
2023-05-18 18:20:16 -0300

    form funcional

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ window.addEventListener('scroll', function() {
   lastScrollTop = scrollTop;
 });
 
-//formulario//
+//formulario validacion//
 
 document.getElementById("botonEnviar").addEventListener("click", function() {
   
@@ -33,4 +33,19 @@ document.getElementById("botonEnviar").addEventListener("click", function() {
   nombreInput.value = "";
   emailInput.value = "";
   mensajeInput.value = "";
+});
+
+//enviar formulario//
+
+document.getElementById("botonEnviar").addEventListener("click", function() {
+  var nombreInput = document.querySelector("#formulario input[name='nombre']");
+  var emailInput = document.querySelector("#formulario input[name='email']");
+  var mensajeInput = document.querySelector("#formulario input[name='mensaje']");
+  
+  var contenidoCorreo = "Nombre: " + nombreInput.value + "%0D%0A";
+  contenidoCorreo += "Email: " + emailInput.value + "%0D%0A";
+  contenidoCorreo += "Mensaje: " + mensajeInput.value + "%0D%0A";
+  
+  var enlaceCorreo = "mailto:mariaevanenning@gmail.com?subject=Formulario de contacto&body=" + contenidoCorreo;
+  window.location.href = enlaceCorreo;
 });
